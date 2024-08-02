9ef88b8ce741d58c9d1a4548bf9a88f6da48b6a4
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-08-02 19:21:11 +0800

    vscode

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-﻿#include<base/task/CancellationTokenSource.h>
-#include<CLI/CLI.hpp>
-#include<iostream>
-#include<jccpp/stream/FileStream.h>
-#include<tsduck/io/TSPacketStreamReader.h>
-#include<tsduck/TSDumper.h>
+﻿#include <CLI/CLI.hpp>
+#include <base/stream/FileStream.h>
+#include <base/task/CancellationTokenSource.h>
+#include <iostream>
+#include <tsduck/TSDumper.h>
+#include <tsduck/io/TSPacketStreamReader.h>
 
 using namespace video;
 using namespace std;
@@ -12,25 +12,23 @@ int main(int argc, char **argv)
 {
 	string input_file_path;
 	string output_file_path;
-	CLI::App app { "输出 ts 文件的包、表格的信息到指定的文本文件中。" };
+	CLI::App app{"输出 ts 文件的包、表格的信息到指定的文本文件中。"};
 
 	try
 	{
 		// 定义输入文件选项
 		app.add_option(
-			"-i,--input",
-			input_file_path,
-			"输入的 ts 文件"
-		)
+			   "-i,--input",
+			   input_file_path,
+			   "输入的 ts 文件")
 			->required()
 			->check(CLI::ExistingFile); // 确保输入文件存在
 
 		// 定义输出文件选项
 		app.add_option(
-			"-o,--output",
-			output_file_path,
-			"输出的文本文件"
-		)
+			   "-o,--output",
+			   output_file_path,
+			   "输出的文本文件")
 			->required();
 
 		app.parse(argc, argv);
@@ -38,9 +36,9 @@ int main(int argc, char **argv)
 		// 执行到这里就是解析命令行成功，并且命令行参数不是 -h,--help
 		cout << "正在解析 ts" << endl;
 
-		shared_ptr<jccpp::FileStream> input_file_stream = jccpp::FileStream::OpenReadOnly(input_file_path);
-		TSPacketStreamReader reader { input_file_stream };
-		shared_ptr<TSDumper> ts_dumper { new TSDumper { output_file_path } };
+		shared_ptr<base::FileStream> input_file_stream = base::FileStream::OpenReadOnly(input_file_path);
+		TSPacketStreamReader reader{input_file_stream};
+		shared_ptr<TSDumper> ts_dumper{new TSDumper{output_file_path}};
 		base::CancellationTokenSource cancel_pump_source;
 		reader.PumpTo(ts_dumper, cancel_pump_source.Token());
 		ts_dumper->DisplayStatisticalResults();
