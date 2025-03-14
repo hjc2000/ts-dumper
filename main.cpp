a090a58feb546eed3030c6fc94b59cf4b75af5bb
hjc2000 <hjc.666@foxmail.com>
2025-03-14 11:44:41 +0800

    命名空间

--- main.cpp
+++ main.cpp
@@ -5,13 +5,10 @@
 #include <tsduck/io/TSPacketStreamReader.h>
 #include <tsduck/TSDumper.h>
 
-using namespace video;
-using namespace std;
-
 int main(int argc, char **argv)
 {
-	string input_file_path;
-	string output_file_path;
+	std::string input_file_path;
+	std::string output_file_path;
 	CLI::App app{"输出 ts 文件的包、表格的信息到指定的文本文件中。"};
 
 	try
@@ -34,11 +31,11 @@ int main(int argc, char **argv)
 		app.parse(argc, argv);
 
 		// 执行到这里就是解析命令行成功，并且命令行参数不是 -h,--help
-		cout << "正在解析 ts" << endl;
+		std::cout << "正在解析 ts" << std::endl;
 
 		shared_ptr<base::IFileStream> input_file_stream = base::di::file::OpenReadOnly(input_file_path);
-		TSPacketStreamReader reader{input_file_stream};
-		shared_ptr<TSDumper> ts_dumper{new TSDumper{output_file_path}};
+		video::TSPacketStreamReader reader{input_file_stream};
+		shared_ptr<video::TSDumper> ts_dumper{new video::TSDumper{output_file_path}};
 		base::CancellationTokenSource cancel_pump_source;
 		reader.PumpTo(ts_dumper, cancel_pump_source.Token());
 		ts_dumper->DisplayStatisticalResults();
@@ -49,7 +46,7 @@ int main(int argc, char **argv)
 	}
 	catch (std::exception &e)
 	{
-		cerr << e.what() << endl;
+		std::cerr << e.what() << std::endl;
 	}
 
 	return 0;
